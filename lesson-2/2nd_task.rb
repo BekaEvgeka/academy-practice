da38609f6764ba22ff2fee844705f32d21b4c40d
BekaEvgeka <outbananalaw@gmail.com>
2024-05-25 01:40:51 +0300

    linted with rubocop

--- lesson-2/2nd_task.rb
+++ lesson-2/2nd_task.rb
@@ -1,23 +1,16 @@
+# frozen_string_literal: true
 
 def foobar
-
-    puts "Enter first number:"
-    first_num = gets.chomp
-
-    puts "Enter second number:"
-    second_num = gets.chomp
-    
-    puts "Output is:"
-    if (first_num.to_i == 20) || (second_num.to_i == 20)
-
-        puts second_num
-
-    else 
-
-        puts first_num.to_i + second_num.to_i
-        
-    end
-
+  puts 'Enter first number:'
+  first_num = gets.chomp.to_i
+  puts 'Enter second number:'
+  second_num = gets.chomp.to_i
+  puts 'Output is:'
+  if (first_num == 20) || (second_num == 20)
+    puts second_num
+  else
+    puts first_num + second_num
+  end
 end
 
-foobar()
+foobar
--- lesson-2/greeting.rb
+++ lesson-2/greeting.rb
@@ -1,24 +1,17 @@
+# frozen_string_literal: true
 
 def greeting
-
-    puts "Enter your name:"
-    name = gets.chomp
-    
-    puts "Enter your surname:"
-    surname = gets.chomp
-
-    puts "Enter your age:"
-    age = gets.chomp
-
-    if age.to_i >= 18
-        
-        puts "Hi, #{name} #{surname}. Time to get to work!"
-        
-    else
-        
-        puts "Hi, #{name} #{surname}. It would seem you are younger than 18, but its never too early to start learning!"
-
-    end
+  puts 'Enter your name:'
+  name = gets.chomp
+  puts 'Enter your surname:'
+  surname = gets.chomp
+  puts 'Enter your age:'
+  age = gets.chomp
+  if age.to_i >= 18
+    puts "Hi, #{name} #{surname}. Time to get to work!"
+  else
+    puts "Hi, #{name} #{surname}. It would seem you are younger than 18, but its never too early to start learning!"
+  end
 end
 
-greeting()
+greeting
