9fe4e0ed3e81cd4a5308608c2edbe8b51474717b
BekaEvgeka <outbananalaw@gmail.com>
2024-05-25 01:56:41 +0300

    linted with rubocop

--- lesson-3/task1/spec/spec_check_word.rb
+++ lesson-3/task1/spec/spec_check_word.rb
@@ -1,24 +1,23 @@
+# frozen_string_literal: true
 
 require_relative '../task1'
 require 'rspec'
 
 describe 'Test check_word' do
+  it 'Word ends with cs' do
+    allow_any_instance_of(Kernel).to recieve(:gets).and_return('Forensics')
+    expect(check_word).to eq(512)
+  end
 
-    it 'Word ends with cs' do
-        allow_any_instance_of(Kernel).to recieve(:gets).and_return('Forensics')
-        expect(check_word).to eq(512)
-    end
+  it 'Word doesnt end with cs' do
+    allow_any_instance_of(Kernel).to recieve(:gets).and_return('Abobus')
 
-    it 'Word doesnt end with cs' do
-        allow_any_instance_of(Kernel).to recieve(:gets).and_return('Abobus')
+    expect(check_word).to eq('subobA')
+  end
 
-        expect(check_word).to eq('subobA')
+  it 'Empty string' do
+    allow_any_instance_of(Kernel).to recieve(:gets).and_return('')
 
-    end
-
-    it 'Empty string' do 
-        allow_any_instance_of(Kernel).to recieve(:gets).and_return('')
-
-        expect(check_word).to eq('')
-    end
+    expect(check_word).to eq('')
+  end
 end
--- lesson-3/task1/task1.rb
+++ lesson-3/task1/task1.rb
@@ -1,17 +1,17 @@
+# frozen_string_literal: true
 
 def check_word(word)
-   
-    if word.downcase.end_with?("cs")
-        
-        return 2 ** word.length
-        
-    else 
+  if word.downcase.end_with?('cs')
 
-        return word.reverse
-        
-    end
+    2**word.length
+
+  else
+
+    word.reverse
+
+  end
 end
 
-puts "Enter your word:"
+puts 'Enter your word:'
 word = gets.chomp
 puts check_word(word)
