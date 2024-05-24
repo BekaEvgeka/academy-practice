357b024c4906abe115ce0a065fcf19c17f5ac3c1
BekaEvgeka <outbananalaw@gmail.com>
2024-05-25 02:02:32 +0300

    linted with rubocop

--- lesson-4/task1/task1.rb
+++ lesson-4/task1/task1.rb
@@ -1,81 +1,62 @@
+# frozen_string_literal: true
 
-
-FILE_PATH = "file.txt"
-BUFFER = "buffer.txt"
-
+FILE_PATH = 'file.txt'
+BUFFER = 'buffer.txt'
 
 def index(file_path)
-    
-    file_data = File.read(file_path)
+  file_data = File.read(file_path)
 
-    puts file_data
-
+  puts file_data
 end
 
 def find(id, file_path)
+  file_data = File.readlines(file_path).map(&:chomp)
 
-    file_data = File.readlines(file_path).map(&:chomp)
-
-    puts file_data[id]
+  puts file_data[id]
 end
 
 def where(pattern, file_path)
+  matched_lines = []
 
-    matched_lines = []
+  File.foreach(file_path) do |line|
+    matched_lines.push(line.chomp) if line.include? pattern
+  end
 
-    File.foreach(file_path) do |line| 
-
-        matched_lines.push(line.chomp) if line.include? pattern
-    
-    end
-
-    p matched_lines
-    
+  p matched_lines
 end
 
 def update(id, text, file_path, buffer_path)
-
-    file = File.open(buffer_path, 'w')
-    File.foreach(file_path).with_index do |line, index|
-        if id == index
-            file.puts(text)
-        else
-            file.puts(line)
-        end
+  file = File.open(buffer_path, 'w')
+  File.foreach(file_path).with_index do |line, index|
+    if id == index
+      file.puts(text)
+    else
+      file.puts(line)
     end
+  end
 
-    file.close
+  file.close
 
-    File.write(file_path, File.read(buffer_path))
+  File.write(file_path, File.read(buffer_path))
 
-    File.delete(buffer_path) if File.exist?(buffer_path)
+  File.delete(buffer_path) if File.exist?(buffer_path)
 end
 
 def delete(id, file_path, buffer_path)
+  file = File.open(buffer_path, 'w')
+  File.foreach(file_path).with_index do |line, index|
+    file.puts(line) unless id == index
+  end
+  file.close
 
-    file = File.open(buffer_path, 'w')
-    File.foreach(file_path).with_index do |line, index|
-        if id == index
+  File.write(file_path, File.read(buffer_path))
 
-        else
-            file.puts(line)
-        end
-    end
-    file.close
-    
-    File.write(file_path, File.read(buffer_path))
-
-    File.delete(buffer_path) if File.exist?(buffer_path)
-
+  File.delete(buffer_path) if File.exist?(buffer_path)
 end
 
 def create(text, file_path)
+  file = File.open(file_path, 'a')
+  file.puts(text)
 
-    file = File.open(file_path, 'a')
-    file.puts(text)
-
-    file.close
-
-
+  file.close
 end
-
--- lesson-4/task2/task2.rb
+++ lesson-4/task2/task2.rb
@@ -1,68 +1,54 @@
-FILE_PATH = "students.txt"
-BUFFER = "buffer.txt"
-RESULT = "results.txt"
+# frozen_string_literal: true
+
+FILE_PATH = 'students.txt'
+BUFFER = 'buffer.txt'
+RESULT = 'results.txt'
 
 def delete(lines, file_path, buffer_path)
-
-    file = File.open(buffer_path, 'w')
-    File.foreach(file_path) do |line|
-
-        unless lines.include? line.chomp
-            file.puts(line)
-        end
-    end
-    file.close
-    File.write(file_path, File.read(buffer_path))
-    File.delete(buffer_path) if File.exist?(buffer_path)
-
+  file = File.open(buffer_path, 'w')
+  File.foreach(file_path) do |line|
+    file.puts(line) unless lines.include? line.chomp
+  end
+  file.close
+  File.write(file_path, File.read(buffer_path))
+  File.delete(buffer_path) if File.exist?(buffer_path)
 end
 
-
 def find_and_write_to_result(pattern, file_path, result_path)
+  result_file = File.open(result_path, 'a')
+  matched_lines = []
 
-    result_file = File.open(result_path, 'a')
-    matched_lines = []
+  File.foreach(file_path) do |line|
+    matched_lines.push(line.chomp) if line.include? pattern.to_s
+    result_file.puts(line) if line.include? pattern.to_s
+  end
+  p matched_lines
+  delete(matched_lines, FILE_PATH, BUFFER)
 
-    File.foreach(file_path) do |line| 
-
-        matched_lines.push(line.chomp) if line.include? pattern.to_s
-        result_file.puts(line) if line.include? pattern.to_s
-    
-    end
-    p matched_lines
-    delete(matched_lines, FILE_PATH, BUFFER)
-
-    result_file.close
-    return matched_lines
-    
+  result_file.close
+  matched_lines
 end
 
 def index(file_path)
-    
-    file_data = File.read(file_path)
+  file_data = File.read(file_path)
 
-    puts file_data
-
+  puts file_data
 end
-
 
 loop do
-
-    if File.zero?(FILE_PATH)
-        puts "No more students, terminating the programm...."
-        index(RESULT)
-        File.write(FILE_PATH, File.read("students_base.txt"))
-        break
-    end
-    puts "Enter student age:"
-    student_age = gets.chomp
-    if student_age.to_i == -1
-        index(RESULT)
-        break
-    else
-        # implement other pattern matching???
-        find_and_write_to_result(student_age.to_i, FILE_PATH, RESULT)
-    end
-
+  if File.zero?(FILE_PATH)
+    puts 'No more students, terminating the programm....'
+    index(RESULT)
+    File.write(FILE_PATH, File.read('students_base.txt'))
+    break
+  end
+  puts 'Enter student age:'
+  student_age = gets.chomp
+  if student_age.to_i == -1
+    index(RESULT)
+    break
+  else
+    # implement other pattern matching???
+    find_and_write_to_result(student_age.to_i, FILE_PATH, RESULT)
+  end
 end
-
